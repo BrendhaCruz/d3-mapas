631918aeb89f8c7c3085fdefe5fce89b4fd3c104
Brendha Cruz Silva 21111905 <brendhacs@lcc3-012.lcc.ufcg.edu.br>
2018-02-22 11:16:37 -0300

    dados de criancas registradas

--- comandos.sh
+++ comandos.sh
@@ -1,3 +1,7 @@
+
+
+
+
 mkdir mapa-ninja
 cd mapa-ninja
 curl https://raw.githubusercontent.com/nazareno/intro-d3-mapas/master/dados/pb_setores_censitarios.zip > malha.zip
@@ -27,16 +31,16 @@ mkdir dados
 unzip dados.zip -d dados
 rm dados.zip
 
-./node_modules/.bin/dsv2json -r ';' -n < "dados/PB/Base informaçoes setores2010 universo PB/CSV/Basico_PB.csv" > pb-censo.ndjson
+./node_modules/.bin/dsv2json -r ';' -n < "dados/PB/Base informaçoes setores2010 universo PB/CSV/Pessoa10_PB.csv" > pb-censo.ndjson
 
-./node_modules/.bin/dsv2json --input-encoding latin1 -r ';' -n < "dados/PB/Base informaçoes setores2010 universo PB/CSV/Basico_PB.csv" > pb-censo.ndjson
+./node_modules/.bin/dsv2json --input-encoding latin1 -r ';' -n < "dados/PB/Base informaçoes setores2010 universo PB/CSV/Pessoa10_PB.csv" > pb-censo.ndjson
 
 ./node_modules/.bin/ndjson-map 'd.Cod_setor = d.properties.CD_GEOCODI, d' < pb-manip.ndjson > pb-ortho-sector.ndjson
 
-./node_modules/.bin/ndjson-join 'd.Cod_setor' pb-ortho-sector.ndjson pb-censo.ndjson > pb-result.ndjson
+./node_modules/.bin/ndjson-join 'd.Cod_setor' pb-ortho-sector.ndjson pb-censo.ndjson > pb-registro.ndjson
 
 
-./node_modules/.bin/ndjson-map 'd[0].properties = {renda: Number(d[1].V005.replace(",", "."))}, d[0]' < pb-result.ndjson > pb-ortho-comdado.ndjson
+./node_modules/.bin/ndjson-map 'd[0].properties = {registro: Number(d[1].V002.replace(",", "."))}, d[0]' < pb-registro.ndjson > pb-ortho-comdado.ndjson
 
 npm install topojson
 
@@ -48,4 +52,4 @@ npm install d3
 
 npm install d3-scale-chromatic
 
-./node_modules/.bin/topo2geo tracts=- < pb-quantized-topo.json | ./node_modules/.bin/ndjson-map -r d3 'z = d3.scaleSequential(d3.interpolateViridis).domain([0, 1e3]), d.features.forEach(f => f.properties.fill = z(f.properties.renda)), d' | ./node_modules/.bin/ndjson-split 'd.features' | ./node_modules/.bin/geo2svg -n --stroke none -w 1000 -h 600 > pb-tracts-threshold-light.svg
+./node_modules/.bin/topo2geo tracts=- < pb-quantized-topo.json | ./node_modules/.bin/ndjson-map -r d3 'z = d3.scaleSequential(d3.interpolateViridis).domain([0, 1e3]), d.features.forEach(f => f.properties.fill = z(f.properties.registro)), d' | ./node_modules/.bin/ndjson-split 'd.features' | ./node_modules/.bin/geo2svg -n --stroke none -w 1000 -h 600 > pb-tracts-threshold-light.svg

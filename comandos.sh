434fe9ef6c79d66aeddfc1acd6854bdb49712e19
Brendha Cruz Silva 21111905 <brendhacs@lcc3-012.lcc.ufcg.edu.br>
2018-02-22 11:40:29 -0300

    dados de criancas registradas 1,2 e 3

--- comandos.sh
+++ comandos.sh
@@ -1,35 +1,46 @@
 
 
 
+#inicia diretorio mapa
+#mkdir mapa-ninja
+#cd mapa-ninja
 
-mkdir mapa-ninja
-cd mapa-ninja
-curl https://raw.githubusercontent.com/nazareno/intro-d3-mapas/master/dados/pb_setores_censitarios.zip > malha.zip
+#baixa dados para a construcao espacial do mapa
+#curl https://raw.githubusercontent.com/nazareno/intro-d3-mapas/master/dados/pb_setores_censitarios.zip > malha.zip
 
-unzip malha.zip
+#unzipa dados
+#unzip malha.zip
 
+#instala shapefile
+#npm install shapefile
 
-npm install shapefile
-
+#inicia o shp
 ./node_modules/.bin/shp2json 25SEE250GC_SIR.shp -o pb.json
 
+#instala a projecao geo
+#npm install d3-geo-projection
 
-npm install d3-geo-projection
-
+#inicializa o geo
 ./node_modules/.bin/geoproject 'd3.geoOrthographic().rotate([54, 14, -2]).fitSize([1000, 600], d)' < pb.json > pb-ortho.json
 
+#dimensoes
 ./node_modules/.bin/geo2svg -w 1000 -h 600 < pb-ortho.json > pb-ortho.svg
+
+#instala o ndjason
+#npm install ndjson-cli
+
+#inicia o ndjason
 ./node_modules/.bin/ndjson-split 'd.features' < pb-ortho.json > pb-manip.ndjson
-npm install ndjson-cli
-./node_modules/.bin/ndjson-split 'd.features' < pb-ortho.json > pb-manip.ndjson
 
-npm install d3-dsv
+#instala o d3-dsv
+#npm install d3-dsv
 
-curl https://raw.githubusercontent.com/nazareno/intro-d3-mapas/master/dados/PB_20171016.zip > dados.zip
+#baixando dados para preencher o mapa
+#curl https://raw.githubusercontent.com/nazareno/intro-d3-mapas/master/dados/PB_20171016.zip > dados.zip
 
-mkdir dados
-unzip dados.zip -d dados
-rm dados.zip
+#mkdir dados
+#unzip dados.zip -d dados
+#rm dados.zip
 
 ./node_modules/.bin/dsv2json -r ';' -n < "dados/PB/Base informaçoes setores2010 universo PB/CSV/Pessoa10_PB.csv" > pb-censo.ndjson
 
@@ -40,16 +51,19 @@ rm dados.zip
 ./node_modules/.bin/ndjson-join 'd.Cod_setor' pb-ortho-sector.ndjson pb-censo.ndjson > pb-registro.ndjson
 
 
-./node_modules/.bin/ndjson-map 'd[0].properties = {registro: Number(d[1].V002.replace(",", "."))}, d[0]' < pb-registro.ndjson > pb-ortho-comdado.ndjson
+./node_modules/.bin/ndjson-map 'd[0].properties = {registro: Number(d[1].V001.replace(",", "."))}, d[0]' < pb-registro.ndjson > pb-ortho-comdado.ndjson
 
-npm install topojson
+#instala o topojason
+#npm install topojson
 
 ./node_modules/.bin/geo2topo -n tracts=pb-ortho-comdado.ndjson > pb-tracts-topo.json
 
 ./node_modules/.bin/toposimplify -p 1 -f < pb-tracts-topo.json | ./node_modules/.bin/topoquantize 1e5 > pb-quantized-topo.json
 
-npm install d3
+#instala o d3
+#npm install d3
 
-npm install d3-scale-chromatic
+#instala o d3 cores
+#npm install d3-scale-chromatic
 
-./node_modules/.bin/topo2geo tracts=- < pb-quantized-topo.json | ./node_modules/.bin/ndjson-map -r d3 'z = d3.scaleSequential(d3.interpolateViridis).domain([0, 1e3]), d.features.forEach(f => f.properties.fill = z(f.properties.registro)), d' | ./node_modules/.bin/ndjson-split 'd.features' | ./node_modules/.bin/geo2svg -n --stroke none -w 1000 -h 600 > pb-tracts-threshold-light.svg
+./node_modules/.bin/topo2geo tracts=- < pb-quantized-topo.json | ./node_modules/.bin/ndjson-map -r d3 'z = d3.scaleSequential(d3.interpolateViridis).domain([0, 500]), d.features.forEach(f => f.properties.fill = z(f.properties.registro)), d' | ./node_modules/.bin/ndjson-split 'd.features' | ./node_modules/.bin/geo2svg -n --stroke none -w 1000 -h 600 > pb-tracts-threshold-light5.svg
